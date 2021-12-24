d6089e129429c39098d0ac5cca55c44204f8a199
TehGM <TehGM@users.noreply.github.com>
2021-12-24 23:07:20 +0100

    Added dockerscript.sh

---
+++ LukeDictionary.Bot/dockerscript.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/bash
+IMAGE="devsub/lukedictionary:latest"
+CONTAINER="LukeDictionary"
+ENVIRONMENT="Production"
+
+echo "Building Docker Image"
+docker build -t $IMAGE .
+
+echo "Stopping old Docker Containers (error might pop, ignore it)"
+docker stop $CONTAINER || true && docker rm $CONTAINER || true
+
+echo "Starting new Docker Container"
+docker run -d --restart=always \
+--name=$CONTAINER \
+-e ASPNETCORE_ENVIRONMENT=$ENVIRONMENT \
+-e DOTNET_ENVIRONMENT=$ENVIRONMENT \
+-v "/var/log/LukeDictionary/$ENVIRONMENT":"/logs" \
+$IMAGE
+
+echo "Pruning old Docker Images"
+docker image prune -f -a
